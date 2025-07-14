fd943744635b366463fbdc48ef260ed6f1ff61a6
SairamNallamothu <sairamnallamothu2294@gmail.com>
2025-07-14 22:11:04 +0200

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,11 @@
+print("AI Generalist")
+print("July 14th 2025")
+print("""I am signing up for Replit's 100 days of Python
+challenge!""")
+print("""I will make sure to spend some time every day coding 
+along, for a minimum of 10 minutes a day. 
+I'll be using Replit, an amazing online IDE so I can do 
+this from my phone wherever I happen to be. No excuses
+for not coding from the middle of a field!
+I am feeling 🔥🔥🔥
+you can follow my progress at replit.com""")
